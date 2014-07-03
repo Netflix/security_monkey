26a63684bf4f1eb9f0a1efe7548141a7f4041438
Patrick Kelley <pkelley@netflix.com>
2014-07-02 23:30:20 -0700

    Audit Emails, Change Notificaitons, and Flask-Security emails may now use SMTP or SES.

--- env-config/config-deploy.py
+++ env-config/config-deploy.py
@@ -29,16 +29,30 @@ WEB_PORT = '443'
 FRONTED_BY_NGINX = True
 NGINX_PORT = '443'
 WEB_PATH = '/static/ui.html'
+BASE_URL = 'https://{}/'.format(FQDN)
 
 SECRET_KEY = '<INSERT_RANDOM_STRING_HERE>'
 
-DEFAULT_MAIL_SENDER = 'securitymonkey@example.com'
+MAIL_DEFAULT_SENDER =  'securitymonkey@example.com'
 SECURITY_REGISTERABLE = True
 SECURITY_CONFIRMABLE = False
 SECURITY_RECOVERABLE = False
 SECURITY_PASSWORD_HASH = 'bcrypt'
 SECURITY_PASSWORD_SALT = '<INSERT_RANDOM_STRING_HERE>'
-SECURITY_POST_LOGIN_VIEW = 'https://ec2-XX-XXX-XXX-XXX.compute-1.amazonaws.com/'
+
+SECURITY_POST_LOGIN_VIEW = BASE_URL
+SECURITY_POST_REGISTER_VIEW = BASE_URL
+SECURITY_POST_CONFIRM_VIEW = BASE_URL
+SECURITY_POST_RESET_VIEW = BASE_URL
+SECURITY_POST_CHANGE_VIEW = BASE_URL
 
 # This address gets all change notifications
 SECURITY_TEAM_EMAIL = ['securityteam@example.com']
+
+# These are only required if using SMTP instead of SES
+EMAILS_USE_SMTP = False     # Otherwise, Use SES
+MAIL_SERVER = 'smtp.example.com'
+MAIL_PORT = 465
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'username'
+MAIL_PASSWORD = 'password'
