3ea3b004259310f29e8020a9f1c2b5f6dadab876
Patrick Kelley <pkelley@netflix.com>
2014-07-04 12:51:58 -0700

    Moved Search/Settings/Signout from Controllers to Components

--- dart/lib/component/signout_component/signout_component.dart
+++ dart/lib/component/signout_component/signout_component.dart
@@ -1,20 +1,20 @@
-library signout_controller;
+library security_monkey.signout_component;
 import 'package:angular/angular.dart';
 import 'package:SecurityMonkey/util/constants.dart';
 
-
-@Controller(
-    selector: '[signout]',
-    publishAs: 'signout_ctrl')
-class SignoutController {
-  // On init, make a call to /logout on API
+@Component(
+    selector: 'signout',
+    templateUrl: 'packages/SecurityMonkey/component/signout_component/signout_component.html',
+    cssUrl: const ['css/bootstrap.min.css'],
+    publishAs: 'cmp')
+class SignoutComponent {
   final Http _http;
   Scope scope;
   bool complete = false;
   bool error = false;
   bool loading = true;
 
-  SignoutController(this._http, this.scope) {
+  SignoutComponent(this._http, this.scope) {
     String url = '$API_HOST/logout';
     print("Signing Out...");
     _http.get(url, withCredentials:true)
@@ -22,9 +22,6 @@ class SignoutController {
       print("Sign Out Complete");
       complete = true;
       loading = false;
-//      scope.emit("username-change", "emit");
-//      scope.broadcast("username-change", "broadcast");
-//      scope.parentScope.broadcast("username-change", "parent-broadcast");
       scope.rootScope.broadcast("username-change", "");
     })
     .catchError((error) {
