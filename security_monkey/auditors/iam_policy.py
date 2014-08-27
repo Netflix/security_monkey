537ac95fc3dd8e53ab4b745e7ab3b5a9ad286468
Patrick Kelley <pkelley@netflix.com>
2014-08-27 16:31:37 -0700

    Extracting statement iteration code into its own function.

--- security_monkey/auditors/iam_policy.py
+++ security_monkey/auditors/iam_policy.py
@@ -34,6 +34,21 @@ class IAMPolicyAuditor(Auditor):
     def __init__(self, accounts=None, debug=False):
         super(IAMPolicyAuditor, self).__init__(accounts=accounts, debug=debug)
 
+    def _iterate_over_statements(self, sub_policies, check_statement_func):
+        """
+        For each Statement in the policy document, execute check_statement_func.
+        Helps when you don't know if the Statement is going to be a list of dicts
+        or a single dict.
+        """
+        for sub_policy_name in sub_policies:
+            sub_policy = sub_policies[sub_policy_name]
+            if type(sub_policy['Statement']) is list:
+                statements = sub_policy['Statement']
+                for statement in statements:
+                    check_statement_func(statement)
+            else:
+                check_statement_func(sub_policy['Statement'])
+
     def library_check_iamobj_has_star_privileges(self, iamobj_item, policies_key='userpolicies'):
         """
         alert when an IAM Object has a policy allowing '*'.
@@ -45,15 +60,7 @@ class IAMPolicyAuditor(Auditor):
                 if statement["Effect"] == "Allow":
                     self.add_issue(10, tag, iamobj_item, notes=json.dumps(statement))
 
-        sub_policies = iamobj_item.config.get(policies_key, {})
-        for sub_policy_name in sub_policies:
-            sub_policy = sub_policies[sub_policy_name]
-            if type(sub_policy['Statement']) is list:
-                statements = sub_policy['Statement']
-                for statement in statements:
-                    check_statement(statement)
-            else:
-                check_statement(sub_policy['Statement'])
+        self._iterate_over_statements(iamobj_item.config.get(policies_key, {}), check_statement)
 
     def library_check_iamobj_has_iam_star_privileges(self, iamobj_item, policies_key='userpolicies'):
         """
@@ -71,15 +78,7 @@ class IAMPolicyAuditor(Auditor):
                     if "Action" in statement and statement["Action"].lower() == "iam:*":
                         self.add_issue(10, tag, iamobj_item, notes=json.dumps(statement))
 
-        sub_policies = iamobj_item.config.get(policies_key, {})
-        for sub_policy_name in sub_policies:
-            sub_policy = sub_policies[sub_policy_name]
-            if type(sub_policy['Statement']) is list:
-                statements = sub_policy['Statement']
-                for statement in statements:
-                    check_statement(statement)
-            else:
-                check_statement(sub_policy['Statement'])
+        self._iterate_over_statements(iamobj_item.config.get(policies_key, {}), check_statement)
 
     def library_check_iamobj_has_iam_privileges(self, iamobj_item, policies_key='userpolicies'):
         """
@@ -97,15 +96,7 @@ class IAMPolicyAuditor(Auditor):
                     if "Action" in statement and statement["Action"].lower().startswith("iam:") and statement["Action"].lower() not in self.explicit_iam_checks:
                         self.add_issue(9, tag, iamobj_item, notes=json.dumps(statement))
 
-        sub_policies = iamobj_item.config.get(policies_key, {})
-        for sub_policy_name in sub_policies:
-            sub_policy = sub_policies[sub_policy_name]
-            if type(sub_policy['Statement']) is list:
-                statements = sub_policy['Statement']
-                for statement in statements:
-                    check_statement(statement)
-            else:
-                check_statement(sub_policy['Statement'])
+        self._iterate_over_statements(iamobj_item.config.get(policies_key, {}), check_statement)
 
     def library_check_iamobj_has_iam_passrole(self, iamobj_item, policies_key='userpolicies'):
         """
@@ -124,15 +115,7 @@ class IAMPolicyAuditor(Auditor):
                     if "Action" in statement and statement["Action"].lower() == "iam:passrole":
                         self.add_issue(9, tag, iamobj_item, notes=json.dumps(statement))
 
-        sub_policies = iamobj_item.config.get(policies_key, {})
-        for sub_policy_name in sub_policies:
-            sub_policy = sub_policies[sub_policy_name]
-            if type(sub_policy['Statement']) is list:
-                statements = sub_policy['Statement']
-                for statement in statements:
-                    check_statement(statement)
-            else:
-                check_statement(sub_policy['Statement'])
+        self._iterate_over_statements(iamobj_item.config.get(policies_key, {}), check_statement)
 
     def library_check_iamobj_has_notaction(self, iamobj_item, policies_key='userpolicies'):
         """
@@ -147,12 +130,4 @@ class IAMPolicyAuditor(Auditor):
                 if "NotAction" in statement:
                     self.add_issue(10, tag, iamobj_item, notes=json.dumps(statement["NotAction"]))
 
-        sub_policies = iamobj_item.config.get(policies_key, {})
-        for sub_policy_name in sub_policies:
-            sub_policy = sub_policies[sub_policy_name]
-            if type(sub_policy['Statement']) is list:
-                statements = sub_policy['Statement']
-                for statement in statements:
-                    check_statement(statement)
-            else:
-                check_statement(sub_policy['Statement'])
+        self._iterate_over_statements(iamobj_item.config.get(policies_key, {}), check_statement)
