ddc89f0fd7c07afc181ae74e13b553f7b137715a
Mark Hillick <markofu@example.com>
2014-09-15 00:22:20 +0100

    Cleaned up a tonne of bugs :)

--- scripts/secmonkey_auto_install.sh
+++ scripts/secmonkey_auto_install.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-################################################################
-# Bash script to automate installation of Security Monkey
+################################################################################################
+# Bash script to automate installation of Security Monkey.
 #
-# Should work for DB installation on RDS and localhost
+# This script assumes that you're going to run Security Monkey on an Ubuntu EC2 Instance in AWS.
+#
+# This script will work for a Postgres DB installation on both RDS and localhost.
+#
+# I'm sure it could be significantly improved on and I'd love feedback on "how" :)
+#
 # Written by :: markofu
-#
-# Version :: 0.1
 #
 # Date :: September 2014
 #
-################################################################
+# To Do :: 
+#         Fix issue with supervisorctl
+#         Clean up redundant keys
+#         Remove requirement to enter password for Postgres
+#         Improve Docs?
+#         Improve as per feedback
+################################################################################################
  
 set -e 
 
@@ -19,7 +28,7 @@ USAGE="Usage: $(basename $0) [-hv] [-d arg] [-e arg] [-i arg] [-n arg] [-p arg] 
 
 For example - 
 
-              bash $(basename $0) -d 10.11.1.11 -e cert_email@secmonkey.com -i 10.10.10.10 -n ec2-10-10-10-10.us-west-1.compute.amazonaws.com -p SuperSecretPasswordYo -r recipient@secmonkey.com -s sender@secmonkey.com -u db_user -w secmonkey.com
+              bash $(basename $0) -d 10.11.1.11 -e cert_email@secmonkey.com -i 10.10.10.10 -n ec2-10-10-10-10.us-west-1.compute.amazonaws.com -p SuperSecretPasswordYo -r recipient@secmonkey.com -s sender@secmonkey.com -u postgres -w secmonkey.com
     
               bash $(basename $0) -h 
 
@@ -27,9 +36,9 @@ For example -
     
 
 CLI switches - 
-              -d  >> IP Address of the Postgres Database
+              -d  >> Hostname or IP Address of the Postgres Database
               -e  >> Email Address used for SSL Cert for the Security Monkey Instance
-              -i  >> IP Address of the SecurityMonkey Instance
+              -i  >> IP Address (what you want the hostname resolve to) of the SecurityMonkey Instance
               -h  >> Prints this message
               -n  >> Hostname of the SecurityMonkey Instance
               -p  >> Password for the Postgres DB on the SecurityMonkey Instance
@@ -43,25 +52,28 @@ CLI switches -
 VERSION="0.1"
 ARGS=$#
 
+err_code=10
 f_hosts="/etc/hosts"
 f_hostname="/etc/hostname"
 f_lsb="/etc/lsb-release"
 f_debug="/var/tmp/sec_monkey_install_debug.log"
-
+f_nginx_site_a="/etc/nginx/sites-available/securitymonkey.conf"
+f_nginx_site_e="/etc/nginx/sites-enabled/securitymonkey.conf"
+f_nginx_default="/etc/nginx/sites-enabled/default"
 
 ### Function to check that the script is being run with options.
 
 check_opt ()
 {
-    if [ $ARGS -eq 0 ];
+    if [ $ARGS -eq 0 ]
     then
         echo -e "\nPlease run with valid options! Help is printed with '-h'.\n"
-        echo -e "\n$USAGE\n";
-        exit 11;
-    elif [ $ARGS -gt 20 ];
+        echo -e "\n$USAGE\n"
+        ($err_code++) && exit $(expr $err_code - 1) 
+    elif [ $ARGS -gt 20 ]
     then
-        echo -e "\nPlease examine the usage options for this script - you can only have a maximum of 7 command line switches!\n" && echo -e "\n$USAGE\n";
-        exit 12;
+        echo -e "\nPlease examine the usage options for this script - you can only have a maximum of 7 command line switches!\n" && echo -e "\n$USAGE\n"
+        ($err_code++) && exit $(expr $err_code - 1) 
     fi
 }
 
@@ -69,11 +81,11 @@ check_opt ()
 
 check_opt_one ()
 {
-    if [ $ARGS -gt 2 ];
+    if [ $ARGS -gt 2 ]
     then 
         echo -e "\nPlease run with only one option! Help is printed with '-h'.\n"
-        echo -e "\n$USAGE\n";
-        exit 13;
+        echo -e "\n$USAGE\n"
+        ($err_code++) && exit $(expr $err_code - 1) 
     fi
 }
 
@@ -84,7 +96,7 @@ parse_arg ()
     for arg in "$@"
     do
         case $arg in
-             -d|--database) # IP Address of the Postgres Database
+             -d|--database) # Hostname or IP Address of the Postgres Database
                  db=$2
                  shift 2
                  ;;
@@ -94,11 +106,10 @@ parse_arg ()
                  ;;
              -h|--help)
                  check_opt_one
-                 echo -e "\n$USAGE\n";
                  exit 0;
                  ;;
              -i|--ip) # IP Address of the SecurityMonkey Instance
-                 pub_ip=$2
+                 real_ip=$2
                  shift 2
                  ;;
              -n|--name) # Hostname of the SecurityMonkey Instance
@@ -118,7 +129,7 @@ parse_arg ()
                  shift 2
                  ;;
              -u|--user) # Postgres DB User
-                 db_user=$2
+                 user=$2
                  shift 2
                  ;;
              -v|--version)
@@ -161,25 +172,26 @@ create_static_var ()
     dir_config="$dir_sm/env-config"  				# Config Directory in Security Monkey
     dir_super="$dir_sm/supervisor"  				# Supervisor Directory in Security Monkey
     dir_nginx_log="/var/log/nginx/log"
+    dir_ssl="/etc/ssl"
     file_deploy="$dir_config/config-deploy.py"
     file_ini="$dir_super/security_monkey.ini"
     file_rc="$HOME/.bashrc"
 
-    if [ -d $dir_sm ];
+    if [ -d $dir_sm ]
     then
         sudo mkdir -p $dir_sm
     else
-        echo -e "\n$dir_sm already exists\n" > $f_debug;
+        echo -e "\n$dir_sm already exists\n" > $f_debug
     fi
     
     export SECURITY_MONKEY_SETTINGS="$file_deploy"		# SECURITY_MONKEY_SETTINGS variable should point to the config-deploy.py file
     exec "$@"
 
-    if grep -Fq "export SECURITY_MONKEY_SETTINGS=" $file_rc;
+    if grep -Fq "export SECURITY_MONKEY_SETTINGS=" $file_rc
     then
-        echo -e "\nEnv Variable SECURITY_MONKEY_SETTINGS already exists in $file_rc\n" >> $f_debug;
+        echo -e "\nEnv Variable SECURITY_MONKEY_SETTINGS already exists in $file_rc\n" >> $f_debug
     else
-        echo -e "\n# Security Monkey Settings\nexport SECURITY_MONKEY_SETTINGS=$file_deploy\n" | sudo tee -ai $file_rc; # Adding to the local .bashrc file
+        echo -e "\n# Security Monkey Settings\nexport SECURITY_MONKEY_SETTINGS=$file_deploy\n" | sudo tee -ai $file_rc # Adding to the local .bashrc file
     fi
     . ~/.bashrc
 }
@@ -188,59 +200,69 @@ create_static_var ()
 
 check_ubuntu ()
 {
-    if grep -Fxq DISTRIB_ID=Ubuntu $f_lsb;
+    if grep -Fxq DISTRIB_ID=Ubuntu $f_lsb
     then
-        echo -e "\nYou're running Ubuntu, gg :)\n"; >> $f_debug;
+        echo -e "\nYou're running Ubuntu, gg :)\n" >> $f_debug
     else
-        echo -e "\nYou don't seem to be running a Ubuntu distro, sorry :( Please only run this on Ubuntu! Now exiting.....\n" && exit 14;
+        echo -e "\nYou don't seem to be running a Ubuntu distro, sorry :( Please only run this on Ubuntu! Now exiting.....\n" && ($err_code++) && exit $(expr $err_code - 1)
     fi
 }
 
 ### Hostnname
 create_host ()
 {
+	echo -e "\nThis Security Monkey instance has a real ip of $real_ip and a hostname of $name....\n"
 	# Checking if the IP of the Security Monkey Instance is in Hosts file
-    if grep -Fq $pub_ip $f_hosts;
+    if grep -Fq $real_ip $f_hosts
     then
-        echo -e "\n$pub_ip is in the $f_hosts file already, moving on.....\n" >> $f_debug;
+        echo -e "\n$real_ip is in the $f_hosts file already, moving on.....\n" >> $f_debug
     else
-        echo -e "\n$pub_ip	$name\n" | sudo tee -ai $f_hosts >> $f_debug;
+        echo -e "\n$real_ip	$name\n" | sudo tee -ai $f_hosts >> $f_debug
     fi
 
     sudo hostname "$name" # Just ensuring that the hostname is correctly set as per the cli prior to install
 
 	# Checking if the hostname of the Security Monkey Instance is in Hostname file
-    if grep -Fqi $name $f_hostname;
+    if grep -Fqi $name $f_hostname
     then
-        echo -e "\nHostname for $pub_ip is $name and is in $f_hostname.\n" >> $f_debug;
+        echo -e "\nHostname for $real_ip is $name and is in $f_hostname.\n" >> $f_debug
     else
-        sudo sed -i.bak -e's/ip.*/\$name/i' $f_hostname;
+        sudo sed -i.bak -e's/ip.*/\$name/i' $f_hostname
     fi
 }
 
 ### Install pre-reqs
-install_pre ()
+
+install_post ()
 {
-   sudo apt-get update
-   sudo apt-get install -y python-pip python-dev python-psycopg2 libpq-dev nginx supervisor git
-   if [[ $db =~ 127.0.0.1 || $db -eq localhost ]];
-   then
-       echo -e "\nInstalling Postgres on the localhost so must NOT be using a RDS, let's hope so.....\n";
-       sudo apt-get install -y postgresql postgresql-contrib 
-   else
-       echo -e "\nNot installing Postgres on the localhost so must be using a RDS, let's hope so.....\n";
-   fi
+    echo -e "\nInstalling Postgres on the localhost so must NOT be using a RDS, let's hope so.....\n"
+    sudo apt-get install -y postgresql postgresql-contrib 
 }
 
-# Create secmonkey db
+install_pre ()
+{
+    sudo apt-get update && sudo apt-get install -y python-pip python-dev python-psycopg2 libpq-dev nginx supervisor git
+    if (($db)) # Checking if the $db variable is an arithmetic operator
+    then
+        [[ $db =~ 127.0.0.1 ]] && install_post
+        echo hello
+    elif [[ $db -eq localhost ]] && echo hello1
+    then
+        install_post
+    else
+       echo -e "\nNot installing Postgres on the localhost so must be using a RDS, let's hope so.....\n"
+    fi
+}
+
+# Connect to the 'postgres' db (assumed to be your default db)
+# Modify user password and create secmonkey db
 create_db ()
 {
-    echo -e "\nWe will now create a Postgres DB user as per your CLI options and you will be prompted for the password....\n"
+    echo -e "We will now create a Postgres DB user as per your CLI options and you will be prompted for the password....\n"
     sleep 3
-    echo "$db_user $db $password"
-    psql -U $db_user -h $db -c "alter user $db_user with password '$password';"
     echo -e "\nWe will now create the _secmonkey_ DB user as per your CLI options and you will be re-prompted for the password....\n"
-    psql -U $db_user -h $db -c 'CREATE DATABASE secmonkey'
+    psql -U $user -d postgres -h $db -c "alter user $user with password '$password';"
+    psql -U $user -h $db -d postgres -c 'CREATE DATABASE secmonkey'
 }
 
 ###
@@ -280,7 +302,7 @@ environment=SECURITY_MONKEY_SETTINGS="$file_deploy"
 command=python $dir_sm/manage.py start_scheduler
 
 directory="$dir_sm"
-environment=PYTHONPATH='\$dir_sm',SECURITY_MONKEY_SETTINGS="$file_deploy"
+environment=PYTHONPATH='$dir_sm/',SECURITY_MONKEY_SETTINGS="$file_deploy"
 user=ubuntu
 autostart=true
 autorestart=true
@@ -314,7 +336,7 @@ WEB_PATH = '/static/ui.html'
 
 SECRET_KEY = 'O3GYKSrqey5SeDhnbcvBNNKl'
 
-DEFAULT_MAIL_SENDER = '\$sender'
+DEFAULT_MAIL_SENDER = '$sender'
 SECURITY_REGISTERABLE = True
 SECURITY_CONFIRMABLE = False
 SECURITY_RECOVERABLE = False
@@ -323,7 +345,7 @@ SECURITY_PASSWORD_SALT = 'gnoSLMMnUIlk2iLhDkW7OgFZ'
 SECURITY_POST_LOGIN_VIEW = 'https://$name'
 
 # This address gets all change notifications
-SECURITY_TEAM_EMAIL = '\$recipient'
+SECURITY_TEAM_EMAIL = '$recipient'
 EOF
 
 }
@@ -333,7 +355,7 @@ EOF
 clone_install ()
 {
     sudo git clone https://github.com/Netflix/security_monkey.git $dir_sm
-
+    sudo chown -R ubuntu:ubuntu $dir_sm
     cd $dir_sm && sudo python setup.py install
 
     create_config_file
@@ -343,27 +365,17 @@ clone_install ()
 }
 
 ### Automating the creation of the self-signed certificate
-# I ripped this off "https://gist.github.com/bradland/1690807", thx :)
-# Script accepts a single argument, the fqdn for the cert
-
-fail_if_error()
-{
-    [ $1 != 0 ] && {
-  	unset PASSPHRASE
-  	exit 15
-  	}
-}
 
 create_ss_cert ()
 {
-    if [ -z "$site" ];
+    if [ -z "$website" ]
     then
-  	    echo -e "Usage: $(basename $0) -s <site>\n";
-  	    exit 16
+        echo -e "\n$USAGE\n"
+  	    ($err_code++) && exit $(expr $err_code - 1)
   	fi
 
 # Generate a passphrase
-export PASSPHRASE=$(head -c 500 /dev/urandom | tr -dc a-z0-9A-Z | head -c 128; echo)
+PASSPHRASE=$(head -c 500 /dev/urandom | tr -dc a-z0-9A-Z | head -c 128; echo)
 
 # Certificate details; replace items in angle brackets with your own info
 subj="
@@ -371,43 +383,45 @@ C=US
 ST=OR
 O=Riot Games
 localityName=PO
-commonName=$site
+commonName=$website
 organizationalUnitName=InfoSec
 emailAddress=$email
 "
 
 # Generate the server private key
-openssl genrsa -des3 -out $site.key -passout env:PASSPHRASE 2048
-fail_if_error $?
-
+echo -e "\nGenerating the server private key.....\n"
+sudo openssl genrsa -des3 -out $website.key -passout pass:$PASSPHRASE 2048
+echo $PASSPHRASE
 # Generate the CSR
-openssl req \
+echo -e "\nGenerating the CSR.....\n"
+sudo openssl req \
     -new \
     -batch \
     -subj "$(echo -n "$subj" | tr "\n" "/")" \
-    -key $site.key \
-    -out $site.csr \
-    -passin env:PASSPHRASE
-fail_if_error $?
-cp $site.key $site.key.org
-fail_if_error $?
+    -key $website.key \
+    -out $website.csr \
+    -passin pass:$PASSPHRASE
+
+cp $website.key $website.key.org
 
 # Strip the password so we don't have to type it every time we restart nginx
-openssl rsa -in $site.key.org -out $site.key -passin env:PASSPHRASE
-fail_if_error $?
+echo -e "\nStriping the password from the site key.....\n"
+sudo openssl rsa -in $website.key.org -out $dir_ssl/private/server.key -passin pass:$PASSPHRASE
 
 # Generate the cert (good for 3 years)
-openssl x509 -req -days 1095 -in $site.csr -signkey $site.key -out $site.crt
-fail_if_error $?
+echo -e "\nGenerating the 3-year cert...\n"
+sudo openssl x509 -req -days 1095 -in $website.csr -signkey $dir_ssl/private/server.key -out $dir_ssl/certs/server.crt
 }
 
 ### this function configures .ini file & starts the supervisor process
 
 cs_supervisor ()
 {
-	create_ini_file
+    create_ini_file
 
     sudo -E supervisord -c $file_ini
+
+    echo -e "\n The next command 'supervisorctl' command will leave you in the 'supervisor' prompt, simply type 'quit' to exit and complete the Security Monkey install....\n" && sleep 3
     sudo -E supervisorctl -c $file_ini
 }
 
@@ -417,10 +431,78 @@ config_nginx ()
     sudo mkdir -p $dir_nginx_log
     sudo touch $dir_nginx_log/securitymonkey.access.log
     sudo touch $dir_nginx_log/securitymonkey.error.log
-    sudo cp $dir_net/securitymonkey.conf /etc/nginx/sites-available/securitymonkey.conf
 
-    sudo ln -s /etc/nginx/sites-available/securitymonkey.conf /etc/nginx/sites-enabled/securitymonkey.conf # Symlink the available site to the enabled site
-    sudo rm /etc/nginx/sites-enabled/default # removing the default site
+    [[ -s $f_nginx_site_a ]] && sudo cp $f_nginx_site_a $f_nginx_site_a.$(date +%Y-%m-%d) && sudo rm $f_nginx_site_a
+
+cat << EOF | sudo tee -ai $f_nginx_site_a
+server {
+   listen      0.0.0.0:443 ssl;
+   ssl_certificate /etc/ssl/certs/server.crt;
+   ssl_certificate_key /etc/ssl/private/server.key;
+   access_log  /var/log/nginx/log/securitymonkey.access.log;
+   error_log   /var/log/nginx/log/securitymonkey.error.log;
+
+    location /register {
+        proxy_read_timeout 120;
+        proxy_pass  http://127.0.0.1:5000;
+        proxy_next_upstream error timeout invalid_header http_500 http_502 http_503 http_504;
+        proxy_redirect off;
+        proxy_buffering off;
+        proxy_set_header        Host            \$host;
+        proxy_set_header        X-Real-IP       \$remote_addr;
+        proxy_set_header        X-Forwarded-For \$proxy_add_x_forwarded_for;
+    }
+
+    location /logout {
+        proxy_read_timeout 120;
+        proxy_pass  http://127.0.0.1:5000;
+        proxy_next_upstream error timeout invalid_header http_500 http_502 http_503 http_504;
+        proxy_redirect off;
+        proxy_buffering off;
+        proxy_set_header        Host            \$host;
+        proxy_set_header        X-Real-IP       \$remote_addr;
+        proxy_set_header        X-Forwarded-For \$proxy_add_x_forwarded_for;
+    }
+
+    location /login {
+        proxy_read_timeout 120;
+        proxy_pass  http://127.0.0.1:5000;
+        proxy_next_upstream error timeout invalid_header http_500 http_502 http_503 http_504;
+        proxy_redirect off;
+        proxy_buffering off;
+        proxy_set_header        Host            \$host;
+        proxy_set_header        X-Real-IP       \$remote_addr;
+        proxy_set_header        X-Forwarded-For \$proxy_add_x_forwarded_for;
+    }
+
+    location /api {
+        proxy_read_timeout 120;
+        proxy_pass  http://127.0.0.1:5000;
+        proxy_next_upstream error timeout invalid_header http_500 http_502 http_503 http_504;
+        proxy_redirect off;
+        proxy_buffering off;
+        proxy_set_header        Host            \$host;
+        proxy_set_header        X-Real-IP       \$remote_addr;
+        proxy_set_header        X-Forwarded-For \$proxy_add_x_forwarded_for;
+    }
+
+    location /static {
+        rewrite ^/static/(.*)$ /$1 break;
+        root /home/ubuntu/security_monkey/security_monkey/static;
+        index ui.html;
+    }
+
+    location / {
+        root /home/ubuntu/security_monkey/security_monkey/static;
+        index ui.html;
+    }
+
+}
+EOF
+
+    echo hello
+    [[ ! -e $f_nginx_site_e ]] && sudo ln -s $f_nginx_site_a $f_nginx_site_e # Symlink the available site to the enabled site
+    [[ -e $f_nginx_default ]] && sudo rm $f_nginx_default # removing the default site
     sudo service nginx restart
     clear_hist
 }
@@ -450,4 +532,3 @@ cs_supervisor
 create_ss_cert
 
 config_nginx
-
