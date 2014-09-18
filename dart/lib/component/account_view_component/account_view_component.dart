b5f90837bfe0c604ddde38b837d0184bdef4675c
Patrick Kelley <pkelley@netflix.com>
2014-09-18 10:24:40 -0700

    Adding Hammock to Account_view_component

--- dart/lib/component/account_view_component/account_view_component.dart
+++ dart/lib/component/account_view_component/account_view_component.dart
@@ -1,8 +1,5 @@
-library security_monkey.account_view_component;
-
-import 'package:angular/angular.dart';
-import 'package:SecurityMonkey/service/account_service.dart';
-import 'package:SecurityMonkey/model/Account.dart';
+//library security_monkey.account_view_component;
+part of security_monkey;
 
 @Component(
     selector: 'accountview',
@@ -10,47 +7,60 @@ import 'package:SecurityMonkey/model/Account.dart';
     cssUrl: const ['css/bootstrap.min.css'],
     publishAs: 'cmp')
 class AccountViewComponent {
-  AccountService as;
   RouteProvider routeProvider;
   Router router;
   Account account;
   bool create = false;
+  bool _as_loaded = false;
+  bool _is_error = false;
+  String err_message = "";
+  ObjectStore store;
+  Scope scope;
 
-  AccountViewComponent(this.as, this.routeProvider, this.router) {
+  AccountViewComponent(this.routeProvider, this.router, this.store, this.scope) {
+    scope.on("globalAlert").listen(this._showMessage);
+
+    this.store = store;
     /* If the URL has an ID, then let's view/edit */
     if (routeProvider.parameters.containsKey("accountid")) {
-      this.as.getAccount(routeProvider.parameters['accountid'])
-        .then((Account account) {
-        this.account = account;
+      store.one(Account, routeProvider.parameters['accountid'])
+      .then((Account account) {
+          this.account = account;
+          _as_loaded = true;
       });
       create = false;
     } else {
-      /* If the URL does not have an ID, then let's create */
+      // If the URL does not have an ID, then let's create
       account = new Account();
       create = true;
     }
   }
 
-  get isLoaded => create || as.isLoaded;
-  get isError => as.isError;
+  get isLoaded => create || _as_loaded;
+  get isError => _is_error;
+
+  void _showMessage(ScopeEvent event) {
+    this._is_error = true;
+    this.err_message = event.data;
+  }
 
   void saveAccount() {
     if (create) {
-      as.createAccount(account).then((Account account) {
-        int id = account.id;
-        router.go('viewaccount', {'accountid':id});
-      });
+        this.store.create(this.account).then((CommandResponse r) {
+          int id = r.content['id'];
+          router.go('viewaccount', {'accountid':id});
+        });
     } else {
-      as.saveAccount(account);
+        this.store.update(this.account);
     }
   }
 
 // Users can just make an account inactive.
 // Not sure if we should expose delete.
   void deleteAccount() {
-    as.deleteAccount(account.id).then((Account account) {
-      router.go('settings', {});
-    });
+      this.store.delete(this.account).then((_) {
+          router.go('settings', {});
+      });
   }
 
 }
