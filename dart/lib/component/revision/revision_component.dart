b9c87c93363c6c0d509a460b71334da622e1a51d
Patrick Kelley <pkelley@netflix.com>
2014-09-24 00:07:55 -0700

    Replaced RevisionService with Hammock and Library -> Part of

--- dart/lib/component/revision/revision_component.dart
+++ dart/lib/component/revision/revision_component.dart
@@ -1,97 +1,98 @@
-library security_monkey.revision_component;
-
-import 'package:angular/angular.dart';
-import 'package:SecurityMonkey/service/revision_comment_service.dart';
-import 'package:SecurityMonkey/service/revision_service.dart';
-import 'package:SecurityMonkey/service/username_service.dart';
-import 'package:SecurityMonkey/model/Revision.dart';
+part of security_monkey;
 
 @Component(
     selector: 'itemrevision',
     templateUrl: 'packages/SecurityMonkey/component/revision/revision_component.html',
     //cssUrl: const ['css/bootstrap.min.css'],
     publishAs: 'cmp',
-    useShadowDom: false
-)
+    useShadowDom: false)
 class RevisionComponent {
-  RevisionCommentService rcs;
-  RevisionService rs;
-  UsernameService us;
-  Revision revision;
-  Revision compare_revision;
-  bool show_diff = false;
+    RevisionCommentService rcs;
+    UsernameService us;
+    ObjectStore store;
+    Revision revision;
+    Revision compare_revision;
+    bool show_diff = false;
 
-  String _ri;
-  @NgAttr('revision_id')
-  set revision_id(ri) {
-    _ri = ri;
-    this.rs.loadData(revision_id).then((new_revision) {
-      revision = new_revision;
-    });
-  }
-  get revision_id => _ri;
+    RevisionComponent(this.store, this.rcs, this.us);
 
-  String _cri;
-  @NgAttr('compare_revision_id')
-  set compare_revision_id(cri) {
-    _cri = cri;
-//    this.rs.loadData(revision_id, compare_revision_id).then((new_revision) {
-//      revision = new_revision;
-//      this.show_diff = true;
-//    });
-  }
-  get compare_revision_id => _cri;
-
-  bool hasDiffHtml() {
-    if (revision != null && revision.diff_html != null)
-      return true;
-    return false;
-  }
-
-  String panelClassForRevision() {
-    if (revision == null || !revision.active) {
-      return "default";
-    } else {
-      return "success";
+    String _ri;
+    @NgAttr('revision_id')
+    set revision_id(ri) {
+        _ri = ri;
+        this.store.one(Revision, ri).then((revision) {
+            this.revision = revision;
+        });
     }
-  }
+    get revision_id => _ri;
 
-  RevisionComponent(this.rs, this.rcs, this.us);
+    String _cri;
+    @NgAttr('compare_revision_id')
+    set compare_revision_id(cri) {
+        _cri = cri;
+    }
+    get compare_revision_id => _cri;
 
-  void set_diff(bool new_diff) {
-      print("Setting diff to $new_diff");
-      if(new_diff) {
-          this.rs.loadData(revision_id, compare_revision_id).then((new_revision) {
-              revision = new_revision;
-              this.show_diff = true;
-          });
-      } else {
-          show_diff = false;
-      }
-  }
+    bool hasDiffHtml() {
+        if (revision != null && revision.diff_html != null) return true;
+        return false;
+    }
 
-  get rev => revision;
-  get user => this.us.name;
+    String panelClassForRevision() {
+        if (revision == null || !revision.active) {
+            return "default";
+        } else {
+            return "success";
+        }
+    }
 
-  String addingComment;
+    void set_diff(bool new_diff) {
+        print("Setting diff to $new_diff");
+        if (new_diff) {
+            store.customQueryOne(Revision,
+                    new CustomRequestParams(
+                            method: "GET",
+                            url:"$API_HOST/revisions/$revision_id?compare=$compare_revision_id",
+                            withCredentials: true
+                            ))
+                   .then( (revision) {
+                this.revision = revision;
+                this.show_diff = true;
+            });
+        } else {
+            show_diff = false;
+        }
+    }
 
-  void addComment() {
-    this.rcs.addComment(int.parse(revision_id), null, true, addingComment)
-      .then( (_) {
-        this.rs.loadData(revision_id, compare_revision_id).then((new_revision) {
-            revision = new_revision;
-            addingComment = "";
+    get rev => revision;
+    get user => this.us.name;
+
+    String addingComment;
+
+    void addComment() {
+        this.rcs.addComment(int.parse(revision_id), null, true, addingComment).then((_) {
+            store.customQueryOne(Revision,
+                    new CustomRequestParams(
+                            method: "GET",
+                            url:"$API_HOST/revisions/$revision_id?compare=$compare_revision_id",
+                            withCredentials: true))
+                   .then( (revision) {
+                this.revision = revision;
+            });
         });
-      });
-  }
+    }
 
-  void removeComment(int comment_id) {
-    this.rcs.addComment(null, comment_id, false, null)
-      .then( (_) {
-        this.rs.loadData(revision_id).then((new_revision) {
-            revision = new_revision;
+    void removeComment(int comment_id) {
+        this.rcs.addComment(null, comment_id, false, null).then((_) {
+            store.customQueryOne(Revision,
+                    new CustomRequestParams(
+                            method: "GET",
+                            url:"$API_HOST/revisions/$revision_id?compare=$compare_revision_id",
+                            withCredentials: true))
+                   .then( (revision) {
+                this.revision = revision;
+            });
         });
-      });
-  }
+    }
 
 }
