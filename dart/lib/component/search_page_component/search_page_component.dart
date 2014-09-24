2c6bf61ef1e467c8b4844f00a6591d4643d1fb6b
Patrick Kelley <pkelley@netflix.com>
2014-09-24 00:06:21 -0700

    Formatting and library->part of

--- dart/lib/component/search_page_component/search_page_component.dart
+++ dart/lib/component/search_page_component/search_page_component.dart
@@ -1,19 +1,18 @@
-library security_monkey.search_page_component;
-
-import 'package:angular/angular.dart';
+part of security_monkey;
 
 @Component(
-  selector: 'search-page',
-  templateUrl: 'packages/SecurityMonkey/component/search_page_component/search_page_component.html',
-  publishAs: 'cmp',
-  useShadowDom: false) // Because a Search-Page contains a Search-Bar,
-  // and a Search-Bar uses JavaScript and is not shadow-dom Compatible,
-  // the Search-Page cannot use shadow-dom.
+    selector: 'search-page',
+    templateUrl: 'packages/SecurityMonkey/component/search_page_component/search_page_component.html',
+    publishAs: 'cmp',
+    useShadowDom: false)
+    // Because a Search-Page contains a Search-Bar,
+    // and a Search-Bar uses JavaScript and is not shadow-dom Compatible,
+    // the Search-Page cannot use shadow-dom.
 class SearchPageComponent {
-  RouteProvider routeProvider;
-  String current_result_type;
+    RouteProvider routeProvider;
+    String current_result_type;
 
-  SearchPageComponent(this.routeProvider) {
-    this.current_result_type = this.routeProvider.route.parent.name;
-  }
+    SearchPageComponent(this.routeProvider) {
+        this.current_result_type = this.routeProvider.route.parent.name;
+    }
 }
