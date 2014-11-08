2b0ffb5841f13cbb53d715a878730f10f031030d
Patrick Kelley <pkelley@netflix.com>
2014-11-07 17:53:54 -0800

    Web UI should redirect to /login by default.
    This can be changed to use an external SAML authenticator by setting
    REMOTE_AUTH to true.  You'll need to update the FQDN param in
    config-deploy so the webapp knows where to redirect when it receives an
    unauthorized response from the API.

--- dart/lib/util/constants.dart
+++ dart/lib/util/constants.dart
@@ -1,10 +1,20 @@
 library security_monkey.constants;
 
+/**
+ ** REMOTE_AUTH:
+ **  FALSE - When the API 403's, just redirect to /login.
+ **   Nginx should proxy the request through to the python login page.
+ **  TRUE - When the API 403's, redirect the browser to whatever URL is provided by the API.
+ **   This is useful for third party authentication like SAML.
+ **/
+
 // LOCAL DEV
 //final String API_HOST = 'http://127.0.0.1:5000/api/1';
+//final bool REMOTE_AUTH = true;
 
 // Same Box
 final String API_HOST = '/api/1';
+final bool REMOTE_AUTH = false;
 
 // Also update a few places in /web/js/searchpage.js
 
