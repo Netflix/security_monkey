fa1b85d1eaa9d3277af48842a047aabbc86ecd6c
Patrick Kelley <opticrealm+cloud9@gmail.com>
2014-11-15 06:07:59 +0000

    Adding SES_REGION config param

--- env-config/config-deploy.py
+++ env-config/config-deploy.py
@@ -51,6 +51,7 @@ SECURITY_TEAM_EMAIL = ['securityteam@example.com']
 
 # These are only required if using SMTP instead of SES
 EMAILS_USE_SMTP = False     # Otherwise, Use SES
+SES_REGION = 'us-east-1'
 MAIL_SERVER = 'smtp.example.com'
 MAIL_PORT = 465
 MAIL_USE_SSL = True
