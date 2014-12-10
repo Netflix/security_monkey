3af6c288516981a23643069bdae107b55d29fcdd
Patrick Kelley <pkelley@netflix.com>
2014-12-09 22:20:50 -0800

    Minor documentation fix

--- security_monkey/auditors/iam_ssl.py
+++ security_monkey/auditors/iam_ssl.py
@@ -12,7 +12,7 @@
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
 """
-.. module: security_monkey.auditors.elb
+.. module: security_monkey.auditors.iam_ssl
     :platform: Unix
 
 .. version:: $$VERSION$$
