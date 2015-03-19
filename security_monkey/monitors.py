3fb8b9abd45f4e86e7eab2c9af674545b93606fe
Patrick Kelley <pkelley@netflix.com>
2015-03-18 17:11:45 -0700

    added a VPC watcher

--- security_monkey/monitors.py
+++ security_monkey/monitors.py
@@ -7,65 +7,82 @@
 .. moduleauthor:: Patrick Kelley <pkelley@netflix.com> @monkeysecurity
 
 """
-from collections import namedtuple
-
-from security_monkey.watchers.sns import SNS, SNSItem
+from security_monkey.watchers.sns import SNS
 from security_monkey.auditors.sns import SNSAuditor
 from security_monkey.watchers.sqs import SQS
 from security_monkey.watchers.keypair import Keypair
-from security_monkey.watchers.iam_role import IAMRole, IAMRoleItem
+from security_monkey.watchers.iam_role import IAMRole
 from security_monkey.auditors.iam_role import IAMRoleAuditor
-from security_monkey.watchers.iam_group import IAMGroup, IAMGroupItem
+from security_monkey.watchers.iam_group import IAMGroup
 from security_monkey.auditors.iam_group import IAMGroupAuditor
-from security_monkey.watchers.iam_user import IAMUser, IAMUserItem
+from security_monkey.watchers.iam_user import IAMUser
 from security_monkey.auditors.iam_user import IAMUserAuditor
-from security_monkey.watchers.security_group import SecurityGroup, SecurityGroupItem
+from security_monkey.watchers.security_group import SecurityGroup
 from security_monkey.auditors.security_group import SecurityGroupAuditor
-from security_monkey.watchers.rds_security_group import RDSSecurityGroup, RDSSecurityGroupItem
+from security_monkey.watchers.rds_security_group import RDSSecurityGroup
 from security_monkey.auditors.rds_security_group import RDSSecurityGroupAuditor
-from security_monkey.watchers.s3 import S3, S3Item
+from security_monkey.watchers.s3 import S3
 from security_monkey.auditors.s3 import S3Auditor
-from security_monkey.watchers.elb import ELB, ELBItem
+from security_monkey.watchers.elb import ELB
 from security_monkey.auditors.elb import ELBAuditor
-from security_monkey.watchers.iam_ssl import IAMSSL, IAMSSLItem
+from security_monkey.watchers.iam_ssl import IAMSSL
 from security_monkey.auditors.iam_ssl import IAMSSLAuditor
-from security_monkey.watchers.redshift import Redshift, RedshiftCluster
+from security_monkey.watchers.redshift import Redshift
 from security_monkey.auditors.redshift import RedshiftAuditor
-from security_monkey.watchers.elastic_ip import ElasticIP, ElasticIPItem
-from security_monkey.watchers.ses import SES, SESItem
+from security_monkey.watchers.elastic_ip import ElasticIP
+from security_monkey.watchers.ses import SES
 from security_monkey.auditors.ses import SESAuditor
+from security_monkey.watchers.vpc.vpc import VPC
+
 
 class Monitor(object):
     """Collects a watcher with the associated auditor"""
-    def __init__(self, index, watcher_class, auditor_class=None, item_class=None):
+    def __init__(self, index, watcher_class, auditor_class=None):
         self.index = index
         self.watcher_class = watcher_class
         self.auditor_class = auditor_class
-        self.item_class = item_class
 
     def has_auditor(self):
-        return self.auditor_class is not None and self.item_class is not None
+        return self.auditor_class is not None
 
 
 __MONITORS = {
-    'sqs':      Monitor('sqs',      SQS,              None,                    None),
-    'elb':      Monitor('elb',      ELB,              ELBAuditor,              ELBItem),
-    'iamssl':   Monitor('iamssl',   IAMSSL,           IAMSSLAuditor,           IAMSSLItem),
-    'rds':      Monitor('rds',      RDSSecurityGroup, RDSSecurityGroupAuditor, RDSSecurityGroupItem),
-    'sg':       Monitor('sg',       SecurityGroup,    SecurityGroupAuditor,    SecurityGroupItem),
-    's3':       Monitor('s3',       S3,               S3Auditor,               S3Item),
-    'iamuser':  Monitor('iamuser',  IAMUser,          IAMUserAuditor,          IAMUserItem),
-    'iamrole':  Monitor('iamrole',  IAMRole,          IAMRoleAuditor,          IAMRoleItem),
-    'iamgroup': Monitor('iamgroup', IAMGroup,         IAMGroupAuditor,         IAMGroupItem),
-    'keypair':  Monitor('keypair',  Keypair,          None,                    None),
-    'sns':      Monitor('sns',      SNS,              SNSAuditor,              SNSItem),
-    'redshift': Monitor('redshift', Redshift,         RedshiftAuditor,         RedshiftCluster),
-    'eip':      Monitor('eip',      ElasticIP,        None,                    ElasticIPItem),
-    'ses':      Monitor('ses',      SES,              SESAuditor,              SESItem)
+    SQS.index:
+        Monitor(SQS.index, SQS, None),
+    ELB.index:
+        Monitor(ELB.index, ELB, ELBAuditor),
+    IAMSSL.index:
+        Monitor(IAMSSL.index, IAMSSL, IAMSSLAuditor),
+    RDSSecurityGroup.index:
+        Monitor(RDSSecurityGroup.index, RDSSecurityGroup, RDSSecurityGroupAuditor),
+    SecurityGroup.index:
+        Monitor(SecurityGroup.index, SecurityGroup, SecurityGroupAuditor),
+    S3.index:
+        Monitor(S3.index, S3, S3Auditor),
+    IAMUser.index:
+        Monitor(IAMUser.index, IAMUser, IAMUserAuditor),
+    IAMRole.index:
+        Monitor(IAMRole.index, IAMRole, IAMRoleAuditor),
+    IAMGroup.index:
+        Monitor(IAMGroup.index, IAMGroup, IAMGroupAuditor),
+    Keypair.index:
+        Monitor(Keypair.index, Keypair, None),
+    SNS.index:
+        Monitor(SNS.index, SNS, SNSAuditor),
+    Redshift.index:
+        Monitor(Redshift.index, Redshift, RedshiftAuditor),
+    ElasticIP.index:
+        Monitor(ElasticIP.index, ElasticIP, None),
+    SES.index:
+        Monitor(SES.index, SES, SESAuditor),
+    VPC.index:
+        Monitor(VPC.index, VPC, None)
 }
+
 
 def get_monitor(monitor_name):
     return __MONITORS.get(monitor_name)
+
 
 def all_monitors():
     return __MONITORS.itervalues()
---
+++ security_monkey/watchers/vpc/vpc.py
@@ -0,0 +1,97 @@
+#     Copyright 2014 Netflix, Inc.
+#
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+"""
+.. module: security_monkey.watchers.vpc
+    :platform: Unix
+
+.. version:: $$VERSION$$
+.. moduleauthor:: Patrick Kelley <pkelley@netflix.com> @monkeysecurity
+
+"""
+
+from security_monkey.watcher import Watcher
+from security_monkey.watcher import ChangeItem
+from security_monkey.constants import TROUBLE_REGIONS
+from security_monkey.exceptions import BotoConnectionIssue
+from security_monkey import app
+
+from boto.vpc import regions
+
+
+class VPC(Watcher):
+    index = 'vpc'
+    i_am_singular = 'VPC'
+    i_am_plural = 'VPCs'
+
+    def __init__(self, accounts=None, debug=False):
+        super(VPC, self).__init__(accounts=accounts, debug=debug)
+
+    def slurp(self):
+        """
+        :returns: item_list - list of VPCs.
+        :returns: exception_map - A dict where the keys are a tuple containing the
+            location of the exception and the value is the actual exception
+
+        """
+        self.prep_for_slurp()
+
+        item_list = []
+        exception_map = {}
+        from security_monkey.common.sts_connect import connect
+        for account in self.accounts:
+            for region in regions():
+                app.logger.debug("Checking {}/{}/{}".format(self.index, account, region.name))
+                try:
+                    conn = connect(account, 'vpc', region=region)
+                    all_vpcs = self.wrap_aws_rate_limited_call(
+                        conn.get_all_vpcs
+                    )
+                except Exception as e:
+                    if region.name not in TROUBLE_REGIONS:
+                        exc = BotoConnectionIssue(str(e), 'vpc', account, region.name)
+                        self.slurp_exception((self.index, account, region.name), exc, exception_map)
+                    continue
+                app.logger.debug("Found {} {}".format(len(all_vpcs), self.i_am_plural))
+                for vpc in all_vpcs:
+
+                    vpc_name = vpc.tags.get(u'Name', None)
+                    vpc_name = "{0} ({1})".format(vpc_name, vpc.id)
+                    if self.check_ignore_list(vpc_name):
+                        continue
+
+                    config = {
+                        "name": vpc.tags.get(u'Name', None),
+                        "id": vpc.id,
+                        "cidr_block": vpc.cidr_block,
+                        "instance_tenancy": vpc.instance_tenancy,
+                        "is_default": vpc.is_default,
+                        "state": vpc.state,
+                        "tags": vpc.tags,
+                        "classic_link_enabled": vpc.classic_link_enabled
+                    }
+
+                    item = VPCItem(region=region.name, account=account, name=vpc_name, config=config)
+                    item_list.append(item)
+
+        return item_list, exception_map
+
+
+class VPCItem(ChangeItem):
+    def __init__(self, region=None, account=None, name=None, config={}):
+        super(VPCItem, self).__init__(
+            index=VPC.index,
+            region=region,
+            account=account,
+            name=name,
+            new_config=config)
