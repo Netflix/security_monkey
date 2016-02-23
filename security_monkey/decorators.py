8bb64af81c780f2a0b8605fda9596b6231eef85e
Patrick Kelley <pkelley@netflix.com>
2016-02-22 17:13:12 -0800

    Removing rate_limited decorator that is already present in botor.

--- security_monkey/decorators.py
+++ security_monkey/decorators.py
@@ -79,46 +79,6 @@ def crossdomain(allowed_origins=None, methods=None, headers=None,
     return decorator
 
 
-def rate_limited(max_attempts=None, max_delay=4):
-    def decorator(f):
-        metadata = {
-            'count': 0,
-            'delay': 0
-        }
-
-        @wraps(f)
-        def decorated_function(*args, **kwargs):
-            def increase_delay(e):
-                print "[rl]"
-                if metadata['delay'] == 0:
-                    metadata['delay'] = 1
-                elif metadata['delay'] < max_delay:
-                    metadata['delay'] *= 2
-
-                if max_attempts and metadata['count'] > max_attempts:
-                    raise e
-
-            metadata['count'] = 0
-            while True:
-                metadata['count'] += 1
-                if metadata['delay'] > 0:
-                    time.sleep(metadata['delay'])
-                try:
-                    retval = f(*args, **kwargs)
-                    metadata['delay'] = 0
-                    return retval
-                except botocore.exceptions.ClientError as e:
-                    if not e.response["Error"]["Code"] == "Throttling":
-                        raise e
-                    increase_delay(e)
-                except boto.exception.BotoServerError as e:
-                    if not e.error_code == 'Throttling':
-                        raise e
-                    increase_delay(e)
-        return decorated_function
-    return decorator
-
-
 def record_exception():
     def decorator(f):
         @wraps(f)
