15f141e2084735ecde6f32bd0c54d1d4298b5723
Steve Nixon <snixon@tenable.com>
2016-04-20 16:25:11 -0700

    Per discussion removing check_securitygroup_any

--- security_monkey/auditors/security_group.py
+++ security_monkey/auditors/security_group.py
@@ -177,20 +177,6 @@ class SecurityGroupAuditor(Auditor):
                     notes = "{} on {}".format(cidr, self.__port_for_rule__(rule))
                     self.add_issue(severity * multiplier, tag, sg_item, notes=notes)
 
-    def check_securitygroup_any(self, sg_item):
-        """
-        Make sure the SG does not contain 0.0.0.0/0
-        """
-        tag = "Security Group contains 0.0.0.0/0"
-        severity = 5
-        multiplier = _check_empty_security_group(sg_item)
-
-        for rule in sg_item.config.get("rules", []):
-            cidr = rule.get("cidr_ip")
-            if "0.0.0.0/0" == cidr:
-                notes = "{} on {}".format(cidr, self.__port_for_rule__(rule))
-                self.add_issue(severity * multiplier, tag, sg_item, notes=notes)
-
     def check_securitygroup_ingress_any(self, sg_item):
         """
         Make sure the SG does not contain any 0.0.0.0/0 ingress rules
