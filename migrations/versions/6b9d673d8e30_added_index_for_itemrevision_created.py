b73265cff5f97a8ebe910ff6a3f0b9269b9650d4
Patrick Kelley <pkelley@netflix.com>
2016-04-20 12:52:20 -0700

    Updating alembic migration to build off of most recent hotfix instead of slightly older DB version

--- migrations/versions/6b9d673d8e30_added_index_for_itemrevision_created.py
+++ migrations/versions/6b9d673d8e30_added_index_for_itemrevision_created.py
@@ -1,14 +1,14 @@
 """Added index for itemrevision created_date
 
 Revision ID: 6b9d673d8e30
-Revises: 1727fb4309d8
+Revises: 2ea41f4610fd
 Create Date: 2016-04-01 09:39:35.148502
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '6b9d673d8e30'
-down_revision = '1727fb4309d8'
+down_revision = '2ea41f4610fd'
 
 from alembic import op
 import sqlalchemy as sa
