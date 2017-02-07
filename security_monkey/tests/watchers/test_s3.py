4b496dad1b8e822eee0cf821f024ebf6d96417db
Patrick Kelley <opticrealm+cloud9@gmail.com>
2017-02-07 21:07:41 +0000

    Moving Mike's S3 watcher into the watchers directory.

--- security_monkey/tests/watchers/test_s3.py
+++ security_monkey/tests/watchers/test_s3.py
@@ -1,10 +1,29 @@
+#     Copyright 2014 Netflix, Inc.
+#
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+"""
+.. module: security_monkey.tests.watchers.test_s3
+    :platform: Unix
+.. version:: $$VERSION$$
+.. moduleauthor::  Mike Grima <mgrima@netflix.com>
+"""
 import boto3
 from moto import mock_s3
 from moto import mock_sts
 
 from security_monkey.watchers.s3 import S3
-from ..datastore import Account, Technology, Item, ExceptionLogs, AccountType
-from . import SecurityMonkeyTestCase, db
+from securitymonkey.datastore import Account, Technology, Item, ExceptionLogs, AccountType
+from security_monkey.tests import SecurityMonkeyTestCase, db
 
 
 class S3TestCase(SecurityMonkeyTestCase):
