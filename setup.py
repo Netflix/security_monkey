67cce4019298ac5f63934e642c3c9cfaf59e8d09
Patrick Kelley <pkelley@netflix.com>
2017-08-10 12:46:16 -0700

    Pinning to idna 2.5 to fix dependency problem with requests.

--- setup.py
+++ setup.py
@@ -64,7 +64,9 @@ setup(
         'jira==0.32',
         'cloudaux>=1.3.3',
         'joblib>=0.9.4',
-        'pyjwt>=1.01'
+        'pyjwt>=1.01',
+        'idna==2.5'  # Pinning to idna to avoid a dependency problem with requests.
+        # First identified as a problem by Qmando - https://github.com/requests/requests/pull/4223
     ],
     extras_require = {
         'onelogin': ['python-saml>=2.2.0'],
