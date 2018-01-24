f56da4e6b0614d4961226bf9f6c6c8836b6ad3fa
Mike Grima <mgrima@netflix.com>
2018-01-24 11:13:00 -0800

    Added celery configs to docker files

--- docker/worker-start.sh
+++ docker/worker-start.sh
@@ -7,4 +7,4 @@ mkdir -p /var/log/security_monkey
 touch /var/log/security_monkey/security_monkey-deploy.log
 
 cd /usr/local/src/security_monkey
-python celery -A security_monkey.task_scheduler.beat.CELERY beat -l debug
+python celery -A celery -A security_monkey.task_scheduler.tasks.CELERY worker -E
