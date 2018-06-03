ff16366972f123293ba1b6fa90ff8208eb15024b
Jordan Milne <jordan.milne@saynotolinux.com>
2018-06-03 08:38:16 -0700

    fixup: invoke header_auth tests in Travis
    SSO tests are not normally called, presumably because of the broken
    Azure AD tests.

--- security_monkey/tests/sso/header_auth.py
+++ security_monkey/tests/sso/header_auth.py
@@ -1,4 +1,4 @@
-#     Copyright (c) 2018 Reddit Inc. All rights reserved.
+#     Copyright 2018 Netflix, Inc.
 #
 #     Licensed under the Apache License, Version 2.0 (the "License");
 #     you may not use this file except in compliance with the License.
