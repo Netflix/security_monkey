c10d074d8dbb72d2ad20a421bb41332dec710ee0
Jordan Milne <jordan.milne@saynotolinux.com>
2018-06-03 09:19:24 -0700

    fixup: docstring

--- security_monkey/tests/sso/header_auth.py
+++ security_monkey/tests/sso/header_auth.py
@@ -12,7 +12,7 @@
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
 """
-.. module: security_monkey.tests.watchers.openstack
+.. module: security_monkey.tests.sso.header_auth
     :platform: Unix
 
 .. version:: $$VERSION$$
