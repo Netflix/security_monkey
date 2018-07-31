deb84880d37ddc5ea6bf62170534c9b27b804532
Dave Greene <dgreene@rapid7.com>
2018-07-31 10:17:05 -0400

    Fixed spacing issue.

--- security_monkey/export/__init__.py
+++ security_monkey/export/__init__.py
@@ -124,14 +124,14 @@ def export_issues():
         query = query.join((ItemRevision, Item.latest_revision_id == ItemRevision.id))
         query = query.filter(ItemRevision.active == active)
     if 'searchconfig' in args:
-	 search = args['searchconfig'].split(',')
-	 conditions = []
-	 for searchterm in search:
-	     conditions.append(ItemAudit.issue.ilike('%{}%'.format(searchterm)))
-	     conditions.append(ItemAudit.notes.ilike('%{}%'.format(searchterm)))
-	     conditions.append(ItemAudit.justification.ilike('%{}%'.format(searchterm)))
-	     conditions.append(Item.name.ilike('%{}%'.format(searchterm)))
-	 query = query.filter(or_(*conditions))
+        search = args['searchconfig'].split(',')
+        conditions = []
+        for searchterm in search:
+            conditions.append(ItemAudit.issue.ilike('%{}%'.format(searchterm)))
+            conditions.append(ItemAudit.notes.ilike('%{}%'.format(searchterm)))
+            conditions.append(ItemAudit.justification.ilike('%{}%'.format(searchterm)))
+            conditions.append(Item.name.ilike('%{}%'.format(searchterm)))
+        query = query.filter(or_(*conditions))
     if 'enabledonly' in args:
         query = query.join((AuditorSettings, AuditorSettings.id == ItemAudit.auditor_setting_id))
         query = query.filter(AuditorSettings.disabled == False)
